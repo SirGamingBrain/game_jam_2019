d712ae2f0dbcb0e2bb6e7624ac254622506dc526
sdavidsonny <sdavidsonny@outlook.com>
2019-10-19 19:01:23 -0400

    SpawnManager Script

---
+++ Assets/Stewart Folder/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] customerPrefabs = new GameObject[5];
+    
+    public Vector3 spawnPoint;
+    public float respawnTime = 5.0f;
+    public float timer;
+    int randomIndex;
+    
+
+    private void Update()
+    {
+        spawnEnemy();
+        randomIndex = Random.Range(0, customerPrefabs.Length);
+    }
+
+    private void spawnEnemy ()
+    {
+        timer += Time.deltaTime;
+        if (timer > respawnTime)
+        {
+
+            //hardcoded position of spawn. Edit it as needed. 
+            //Instantiate(customerPrefabs, spawnPoint, Quaternion.identity);
+            GameObject instantiatedObject = Instantiate(customerPrefabs[randomIndex], spawnPoint, Quaternion.identity);
+           timer = 0;
+        }
+    }
+
+
+}
