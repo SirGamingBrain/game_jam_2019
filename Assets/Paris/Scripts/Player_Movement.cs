6987167cf4940b796a37de11fc262086f7a5372e
OriginalNappyBoi <54566412+OriginalNappyBoi@users.noreply.github.com>
2019-10-18 20:38:58 -0400

    my scene
    my stuff bois check that shit out

---
+++ Assets/Paris/Scripts/Player_Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player;
+
+    public Rigidbody rb;
+
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+    void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right);
+        }
+    }
+}
+
