c78126aec60013dca669086aeed8b37df23ce4ca
SirGamingBrain <chastan72@gmail.com>
2019-10-20 11:41:00 -0400

    3D and 2D Potion Prefabs Complete

--- Assets/Chastan/Scripts/OrderScript.cs
+++ Assets/Chastan/Scripts/OrderScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class OrderScript : MonoBehaviour
 {
+    readonly int maxOrders = 5;
+    int currentOrders = 0;
+    float[] timers = new float[5] {0, 0, 0, 0, 0};
+
     // Start is called before the first frame update
     void Start()
     {
